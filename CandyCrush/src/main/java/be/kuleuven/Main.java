355a64d073acd3541e5896d477192c0f0ce494b4
RaufPere <rauf.pere2002@outlook.com>
2024-02-25 14:25:45 +0100

    code works

--- CandyCrush/src/main/java/be/kuleuven/Main.java
+++ CandyCrush/src/main/java/be/kuleuven/Main.java
@@ -1,8 +1,31 @@
 package be.kuleuven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
+        //Variables
+        int gridWidth = 4;
+        int gridHeight = 4;
+        int indexToCheck = 5;
 
+        //Initialize grid
+        ArrayList<Integer> grid = new ArrayList<>(Arrays.asList(
+                0, 0, 1, 0,
+                1, 1, 0, 2,
+                2, 0, 1, 3,
+                0, 1, 1, 1
+        ));
+
+        //Create object
+        CheckNeighboursInGrid myObj = new CheckNeighboursInGrid();
+
+        //Get neighbour indexes with member function
+        Iterable<Integer> neighboursIds = myObj.getSameNeighboursIds(grid, gridWidth,gridHeight,indexToCheck);
+
+        //Print neighbouring indexes
+        System.out.println(neighboursIds);
     }
 }
