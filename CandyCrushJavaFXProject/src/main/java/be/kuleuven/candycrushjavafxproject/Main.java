6bec70e49c71de5163dd9a14292c7644087bded4
RaufPere <rauf.pere@student.uhasselt.be>
2024-03-06 16:24:27 +0100

    geimporteerd checkneighbours

---
+++ CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/Main.java
@@ -0,0 +1,15 @@
+package be.kuleuven.candycrushjavafxproject;
+
+import be.kuleuven.CheckNeighboursInGrid;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("test");
+
+    }
+}
