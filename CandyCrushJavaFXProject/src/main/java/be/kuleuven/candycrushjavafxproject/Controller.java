d429c96af6b91c86dbff01f929df0933e4a7c7c7
RaufPere <rauf.pere@student.uhasselt.be>
2024-03-10 09:35:36 +0100

    Grid randomizer werkt

--- CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/Controller.java
+++ CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Controller extends Application {
@@ -20,9 +21,16 @@ public class Controller extends Application {
     public TextField NameTextBox;
     @FXML
     public Label NameLabel;
+    @FXML
+    private Label   lbl1x1,lbl1x2,lbl1x3,lbl1x4,
+                    lbl2x1,lbl2x2,lbl2x3,lbl2x4,
+                    lbl3x1, lbl3x2, lbl3x3, lbl3x4,
+                    lbl4x1, lbl4x2, lbl4x3, lbl4x4;
+    @FXML
+    public ArrayList<Label> labelGridList = new ArrayList<>();
     public String PlayerName;
-
     private Stage stage;
+    private Model model;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -63,8 +71,19 @@ public class Controller extends Application {
         stage.setResizable(false);
 
         updateNameLabel();
+        loadGridLabelIds();
+
+        model = new Model();
+        model.RandomizeGrid(labelGridList,4*4);
 
         stage.show();
+    }
+
+    public void loadGridLabelIds() {
+        labelGridList.add(lbl1x1); labelGridList.add(lbl1x2); labelGridList.add(lbl1x3); labelGridList.add(lbl1x4);
+        labelGridList.add(lbl2x1); labelGridList.add(lbl2x2); labelGridList.add(lbl2x3); labelGridList.add(lbl2x4);
+        labelGridList.add(lbl3x1); labelGridList.add(lbl3x2); labelGridList.add(lbl3x3); labelGridList.add(lbl3x4);
+        labelGridList.add(lbl4x1); labelGridList.add(lbl4x2); labelGridList.add(lbl4x3); labelGridList.add(lbl4x4);
     }
 
     public void updateNameLabel() {
--- CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/Model.java
+++ CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/Model.java
@@ -1,8 +1,18 @@
 package be.kuleuven.candycrushjavafxproject;
 
-public class Model {
-    public void RandomizeGrid()
-    {
+import javafx.scene.control.Label;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Model {
+    public void RandomizeGrid(ArrayList<Label> labelGridList,int nElements)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < nElements; i++) {
+            int randomNumber = random.nextInt(5) + 1;
+            labelGridList.get(i).setText(String.valueOf(randomNumber));
+        }
     }
 }
