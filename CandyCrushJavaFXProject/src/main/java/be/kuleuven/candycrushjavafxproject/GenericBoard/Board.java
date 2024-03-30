9347f6e38c3305204fa05ce7d18c05128fc932a3
RaufPere <rauf.pere@student.uhasselt.be>
2024-03-30 13:26:46 +0100

    hashmap gezet in board

--- CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/GenericBoard/Board.java
+++ CandyCrushJavaFXProject/src/main/java/be/kuleuven/candycrushjavafxproject/GenericBoard/Board.java
@@ -1,32 +1,35 @@
 package be.kuleuven.candycrushjavafxproject.GenericBoard;
 
 import be.kuleuven.candycrushjavafxproject.BoardSize;
+import be.kuleuven.candycrushjavafxproject.Candies.Candy;
 import be.kuleuven.candycrushjavafxproject.Position;
 import javafx.geometry.Pos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Board <T>{
 
-    private ArrayList<T> gridData;
-    private int rows;
-    private int cols;
-    private BoardSize boardSize;
+    private final HashMap<Position, T> gridData;
+    private final int rows;
+    private final int cols;
+    private final BoardSize boardSize;
 
     public Board(BoardSize boardSize){
-        gridData = new ArrayList<>();
+        gridData = new HashMap<>();
         this.rows = boardSize.rows();
         this.cols = boardSize.cols();
         this.boardSize = boardSize;
     }
 
     public T getCellAt(Position position){
-        return gridData.get(position.toIndex());
+        return gridData.get(position);
     }
 
     public void replaceCellAt(Position position,T newCell){
-        gridData.set(position.toIndex(), newCell);
+        gridData.put(position, newCell);
     }
 
     public void fill(Function<Position, T> cellCreator){
@@ -36,7 +39,7 @@ public class Board <T>{
             for (int j = 0; j < cols; j++){
                 Position currentPosition = new Position(i+1,j+1,boardSize);
                 T cell = cellCreator.apply(currentPosition);
-                gridData.add(cell);
+                gridData.put(currentPosition, cell);
             }
         }
     }
@@ -46,8 +49,12 @@ public class Board <T>{
             throw new IllegalArgumentException("The dimensions of the current grid and the received grid arent the same.");
         }
 
-        for (int i = 0; i < rows * cols; i++) {
-            otherBoard.gridData.add(this.gridData.get(i));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Position currentPosition = new Position(i + 1,j+1, boardSize);
+
+                otherBoard.gridData.put(currentPosition,this.gridData.get(currentPosition));
+            }
         }
     }
 }
